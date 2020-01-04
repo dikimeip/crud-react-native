476d1146d12b754c219f375d019f45ec98983d08
dikimeip <pm159145@gmail.com>
2020-01-04 14:51:14 +0700

    membuat fungsi untuk melakukan ubah data

--- src/Edit.js
+++ src/Edit.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput,TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 class Edit extends Component {
     constructor(props) {
@@ -12,65 +12,86 @@ class Edit extends Component {
         }
     }
 
-    componentDidMount = () => {
-        this.setState({
-            nama: this.props.navigation.state.params.nama,
-            kelas: this.props.navigation.state.params.kelas,
-            nim: this.props.navigation.state.params.nim,
-            alamat: this.props.navigation.state.params.alamat,
-        })
+    updateHandler = () => {
+        fetch("http://192.168.43.230/api_android/update.php", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                nim: this.state.nim,
+                nama: this.state.nama,
+                kelas: this.state.kelas,
+                alamat: this.state.alamat,
+            })
+        }).then(response => response.json())
+            .then(responsejson => {
+                alert(JSON.stringify(responsejson.message))
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+
+        componentDidMount = () => {
+            this.setState({
+                nama: this.props.navigation.state.params.nama,
+                kelas: this.props.navigation.state.params.kelas,
+                nim: this.props.navigation.state.params.nim,
+                alamat: this.props.navigation.state.params.alamat,
+            })
+        }
+
+        render() {
+            return (
+                <View>
+                    <Text style={{ fontSize: 14, marginLeft: 20, marginTop: 20 }} >Nim Mahasiswa</Text>
+                    <TextInput
+                        placeholder='Masukan Nim'
+                        underlineColorAndroid='blue'
+                        style={{ marginTop: 0, textAlign: 'center', height: 40 }}
+                        value={this.state.nim}
+                        editable={false}
+                    />
+                    <Text style={{ fontSize: 14, marginLeft: 20, marginTop: 20 }} >Nama Mahasiswa</Text>
+                    <TextInput
+                        placeholder='Masukan Nama'
+                        underlineColorAndroid='blue'
+                        style={{ marginTop: 20, textAlign: 'center', height: 40 }}
+                        value={this.state.nama}
+                        onChangeText={TextInput => this.setState({ nama: TextInput })}
+                    />
+                    <Text style={{ fontSize: 14, marginLeft: 20, marginTop: 20 }} >Kelas Mahasiswa</Text>
+                    <TextInput
+                        placeholder='Masukan Kelas'
+                        underlineColorAndroid='blue' style={{ marginTop: 20, textAlign: 'center', height: 40 }}
+                        keyboardType="numeric"
+                        value={this.state.kelas}
+                        onChangeText={TextInput => this.setState({ kelas: TextInput })}
+                    />
+                    <Text style={{ fontSize: 14, marginLeft: 20, marginTop: 20 }} >Alamat Mahasiswa</Text>
+                    <TextInput
+                        placeholder='Masukan Alamat'
+                        underlineColorAndroid='blue'
+                        style={{ marginTop: 20, textAlign: 'center', height: 40 }}
+                        value={this.state.alamat}
+                        onChangeText={TextInput => this.setState({ alamat: TextInput })}
+                    />
+                    <TouchableOpacity
+                        onPress={this.updateHandler}
+                        style={{ backgroundColor: '#61A754', padding: 20, borderRadius: 10, marginTop: 20 }}
+                    >
+                        <Text style={{ fontSize: 13, fontWeight: 'bold', color: 'white', textAlign: 'center' }}>UBAH DATA</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+
+                        style={{ backgroundColor: '#61A754', padding: 20, borderRadius: 10, marginTop: 20 }}
+                    >
+                        <Text style={{ fontSize: 13, fontWeight: 'bold', color: 'white', textAlign: 'center' }}>HAPUS DATA</Text>
+                    </TouchableOpacity>
+                </View>
+            )
+        }
     }
 
-    render() {
-        return (
-            <View>
-                <Text style={{ fontSize: 14, marginLeft: 20, marginTop: 20 }} >Nim Mahasiswa</Text>
-                <TextInput
-                    placeholder='Masukan Nim'
-                    underlineColorAndroid='blue'
-                    style={{ marginTop: 0, textAlign: 'center', height: 40 }}
-                    value={this.state.nim}
-                    editable={false}
-                />
-                <Text style={{ fontSize: 14, marginLeft: 20, marginTop: 20 }} >Nama Mahasiswa</Text>
-                <TextInput
-                    placeholder='Masukan Nama'
-                    underlineColorAndroid='blue'
-                    style={{ marginTop: 20, textAlign: 'center', height: 40 }}
-                    value={this.state.nama}
-                    onChangeText={TextInput => this.setState({ nama: TextInput })}
-                />
-                <Text style={{ fontSize: 14, marginLeft: 20, marginTop: 20 }} >Kelas Mahasiswa</Text>
-                <TextInput
-                    placeholder='Masukan Kelas'
-                    underlineColorAndroid='blue' style={{ marginTop: 20, textAlign: 'center', height: 40 }}
-                    keyboardType="numeric"
-                    value={this.state.kelas}
-                    onChangeText={TextInput => this.setState({ kelas: TextInput })}
-                />
-                <Text style={{ fontSize: 14, marginLeft: 20, marginTop: 20 }} >Alamat Mahasiswa</Text>
-                <TextInput
-                    placeholder='Masukan Alamat'
-                    underlineColorAndroid='blue'
-                    style={{ marginTop: 20, textAlign: 'center', height: 40 }}
-                    value={this.state.alamat}
-                    onChangeText={TextInput => this.setState({ alamat: TextInput })}
-                />
-                <TouchableOpacity
-                    onPress={}
-                    style={{ backgroundColor: '#61A754', padding: 20, borderRadius: 10, marginTop: 20 }}
-                >
-                    <Text style={{ fontSize: 13, fontWeight: 'bold', color: 'white', textAlign: 'center' }}>UBAH DATA</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={}
-                    style={{ backgroundColor: '#61A754', padding: 20, borderRadius: 10, marginTop: 20 }}
-                >
-                    <Text style={{ fontSize: 13, fontWeight: 'bold', color: 'white', textAlign: 'center' }}>HAPUS DATA</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
-}
-
-export default Edit
+    export default Edit
